41ba0cb72c5f262b80f0efd0c22c993330cb2972
Nicolas Zermati <nicoolas25@gmail.com>
2021-10-24 11:03:38 +0200

    Concurrency control support for the client

--- flypper/client.py
+++ flypper/client.py
@@ -1,4 +1,5 @@
 from time import monotonic
+from threading import Semaphore
 from typing import Callable, Dict, TYPE_CHECKING
 
 from flypper.context import Context
@@ -20,6 +21,7 @@ class Client:
         self._next_sync: float = 0
         self._flags: Dict[str, "Flag"] = {}
         self._time_fn: Callable[[], float] = time_fn
+        self._semaphore: Semaphore = Semaphore()
 
     def flags(self) -> Dict[str, "Flag"]:
         self._sync()
@@ -29,21 +31,22 @@ class Client:
         return Context(client=self, entries=entries)
 
     def _sync(self):
-        now = self._time_fn()
+        with self._semaphore:
+            now = self._time_fn()
 
-        if now < self._next_sync:
-            return
+            if now < self._next_sync:
+                return
 
-        # Get the latest flags from the backend
-        new_flags = self._storage.list(version__gt=self._last_version)
+            # Get the latest flags from the backend
+            new_flags = self._storage.list(version__gt=self._last_version)
 
-        if new_flags:
-            # Update cache
-            for new_flag in new_flags:
-                if new_flag.is_deleted:
-                    del self._flags[new_flag.name]
-                else:
-                    self._flags[new_flag.name] = new_flag
-            self._last_version = max(flag.version for flag in new_flags)
+            if new_flags:
+                # Update cache
+                for new_flag in new_flags:
+                    if new_flag.is_deleted:
+                        del self._flags[new_flag.name]
+                    else:
+                        self._flags[new_flag.name] = new_flag
+                self._last_version = max(flag.version for flag in new_flags)
 
-        self._next_sync = now + self._ttl
+            self._next_sync = now + self._ttl
